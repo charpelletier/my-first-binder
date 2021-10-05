62ca7840215acf88e495af6c644394754334a7a6
Charles Pelletier <67657160+charpelletier@users.noreply.github.com>
2021-10-05 15:49:18 -0400

    Update and rename hello.py to hello.ipynb

--- hello.py
+++
@@ -1,3 +0,0 @@
-# https://github.com/alan-turing-institute/the-turing-way/blob/master/workshops/boost-research-reproducibility-binder/workshop-presentations/zero-to-binder-python.md#running-code-is-more-complicated-than-displaying-code
-
-print("Hello moi from Binder!") 
