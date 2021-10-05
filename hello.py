126bcea5d36ed49d269e32780b0687091ce786a3
Charles Pelletier <67657160+charpelletier@users.noreply.github.com>
2021-10-05 14:34:42 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,3 @@
+# https://github.com/alan-turing-institute/the-turing-way/blob/master/workshops/boost-research-reproducibility-binder/workshop-presentations/zero-to-binder-python.md#running-code-is-more-complicated-than-displaying-code
+
 print("Hello moi from Binder!") 
